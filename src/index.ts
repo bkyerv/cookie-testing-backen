8e85fab1fafc67de7ea16575a0d15f6b60d38aa6
boulatbek <87478959+bkyerv@users.noreply.github.com>
2023-09-09 21:05:01 +0600

    wip2

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,35 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	URL: string;
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const corsHeaders = {
+			'Access-Control-Allow-Origin': env.URL ? env.URL : 'http://localhost:5173',
+			'Access-Control-Allow-Headers': '*',
+			'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+			'Access-Control-Max-Age': '86400',
+			'Access-Control-Allow-Credentials': 'true',
+		};
+
+		if (request.method === 'OPTIONS') {
+			return new Response(null, {
+				headers: corsHeaders,
+			});
+		}
+		const url = new URL(request.url);
+
+		if (request.method === 'GET' && url.pathname === '/') {
+			const response = new Response('Cookie has been set!', { headers: corsHeaders, status: 200 });
+
+			// Adjust the cookie settings as necessary
+			response.headers.append(
+				'Set-Cookie',
+				`test_cookie_name=sample_cookie_value; SameSite=${env.URL ? 'None;' : 'Lax;'} ${env.URL && 'Secure;'}   Max-Age=86400`
+			);
+
+			return response;
+		}
+		return new Response('endpoint not found', { headers: corsHeaders, status: 404 });
 	},
 };
